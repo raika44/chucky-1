bd248982ed77a7306a530a92bda8704b0d37d274
raika44 <33775480+raika44@users.noreply.github.com>
2017-12-17 22:48:14 +0700

    Update iya.py

--- iya.py
+++ iya.py
@@ -16,7 +16,7 @@ import time,random,sys,json,codecs,threading,glob,urllib,urllib2,urllib3,re,ast,
 
 
 cl = LINETCR.LINE()
-cl.login(token="En5ceXTCZlN9VE53wYlc.eoXp0xK1vqkdnL9Q78yCpa.0TcuENRqIzVQ35VFgNHKipw2Elo+IDJxie3W0rpGfBQ=
+cl.login(token="En5ceXTCZlN9VE53wYlc.eoXp0xK1vqkdnL9Q78yCpa.0TcuENRqIzVQ35VFgNHKipw2Elo+IDJxie3W0rpGfBQ=)
 cl.loginResult()
 
 kc = LINETCR.LINE()
